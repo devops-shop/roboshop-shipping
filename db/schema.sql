490d5213ae97bace6a3c7d6a9122588fa0721e67
Richard Okoro <glasshouse747@gmail.com>
2025-10-24 11:07:05 +0100

    roboshop commit

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS cities;
 
 use cities;
 
-DROP TABLE IF EXISTS `cities`;
+/*DROP TABLE IF EXISTS `cities`;*/;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cities` (
+CREATE TABLE IF NOT EXISTS  `cities` (
   `uuid` int(11) NOT NULL AUTO_INCREMENT,
   `country_code` varchar(2) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
@@ -18,4 +18,3 @@ CREATE TABLE `cities` (
   KEY `c_code_idx` (`country_code`),
   FULLTEXT KEY `city_idx` (`city`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6223666 DEFAULT CHARSET=latin1;
-
